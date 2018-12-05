a3d07a19d05ff541682c197697f27fc6f9c0d741
caiosousa21 <caiofmsousa@gmail.com>
2018-12-05 09:22:45 -0200

    delete do carrinho, bonito

--- src/botoesCard.js
+++ src/botoesCard.js
@@ -3,14 +3,29 @@ export default function botoesCard() {
     let botaoAdd = document.querySelectorAll('button.add');
     let carrinho = document.querySelector('div.contentCarrinho');
     let titulo = document.querySelectorAll('p.nome');
+    
+    
     for (let i = 0; i < botaoAdd.length; i++) {
         botaoAdd[i].addEventListener('click',()=>{
+            
+            let contCarrinho = document.createElement('div');
+            let deleteCont = document.createElement('button');
+
+
+            contCarrinho.className = 'contCarrinho';
+            deleteCont.className = 'deleteCont';
+
             let str = titulo[i].outerHTML;
             str = str.replace(/<p class="nome">/g,'');
             str = str.replace('</p>','');
-            let contCarrinho = document.createElement('div');
-            contCarrinho.className = 'contCarrinho';
-            contCarrinho.innerHTML = str;
+
+            contCarrinho.innerHTML = str + '<br/>';
+            contCarrinho.appendChild(deleteCont);
+
+            deleteCont.addEventListener('click',()=>{
+                 contCarrinho.parentNode.removeChild(contCarrinho);
+             });
+
             carrinho.appendChild(contCarrinho);
         })
     }
